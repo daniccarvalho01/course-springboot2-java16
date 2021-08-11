351f659d1af89a835f7c42e14d96008506ecd71d
Daniela Carvalho <daniela.suppino@mgil.com>
2021-08-11 01:28:54 -0300

    Category

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -1,9 +1,11 @@
 package com.educandoweb.course.config;
 
 
+import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.OrderStatus;
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.CategoryRepository;
 import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +26,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
@@ -37,5 +42,12 @@ public class TestConfig implements CommandLineRunner {
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT,u1);
 
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
     }
 }
--- src/main/java/com/educandoweb/course/entities/OrderStatus.java
+++ src/main/java/com/educandoweb/course/entities/OrderStatus.java
@@ -17,5 +17,14 @@ public enum OrderStatus {
         return code;
     }
 
+    public static OrderStatus valueOf(int code){
+        for (OrderStatus value : OrderStatus.values()){
+            if(value.getCode() == code){
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("Invalid OrderStatus code");
+    }
+
     
 }
